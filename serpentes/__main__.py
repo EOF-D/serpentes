37df0c7bdb0cfd5782f771621cf1687fc023bd33
EOF-D <END-OFD@pm.me>
2023-02-23 17:42:00 -0500

    feat(meta): Add a command to debug lark

--- serpentes/__main__.py
+++ serpentes/__main__.py
@@ -1,11 +1,16 @@
+import os
+
 from argparse import ArgumentParser
 from sysconfig import get_paths
 
+import rich
+from lark import Lark
 from serpentes import __author__, __version__
 
 
 def main() -> None:
     parser = ArgumentParser(prog="Serpentes", description="Tools for Serpentes.")
+    parser.add_argument("--author", action="store_true", help="The author of Serpentes.")
 
     parser.add_argument(
         "--hook", action="store_true", help="Hooks the Serpentes codec to Python."
@@ -17,7 +22,10 @@ def main() -> None:
         help="Shows the current version of Serpentes.",
     )
 
-    parser.add_argument("--author", action="store_true", help="The author of Serpentes.")
+    parser.add_argument(
+        "--lark",
+        help="Run the lark parser on a file."
+    )
 
     args = parser.parse_args()
 
@@ -27,7 +35,7 @@ def main() -> None:
     elif args.author is True:
         print(__author__)
 
-    if args.hook is True:
+    elif args.hook is True:
         path = get_paths()["purelib"]
 
         with open(path + "/serpentes_autoload.pth", "a") as fp:
@@ -35,6 +43,14 @@ def main() -> None:
 
         print(f"Wrote `serpentes_autoload.pth` to {path}")
 
+    elif args.lark:
+        path = os.path.dirname(__file__)
+
+        with open(f"{path}/parser/grammar.lark", "r") as fp:
+            parser = Lark(fp.read(), start="module", parser="earley", propagate_positions=True)
+
+        with open(args.lark, "r") as fp:
+            rich.print(parser.parse(fp.read()))
 
 if __name__ == "__main__":
     main()
