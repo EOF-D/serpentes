a3b7bfad38269cb2d6a0be2d58f662a012adf0a6
EOF-D <END-OFD@pm.me>
2023-02-24 17:33:59 -0500

    feat(parser): Added parsing for containers

--- serpentes/parser/__init__.py
+++ serpentes/parser/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import ast
 import typing
+from itertools import islice
 
 from lark import Token, Transformer, Tree, v_args
 from lark.tree import Meta
@@ -18,12 +19,50 @@ from ..nodes import (
     Variables,
 )
 
+Containers: typing.TypeAlias = (
+    Node[ast.List] | Node[type[ast.Tuple]] | Node[type[ast.Set]] | Node[type[ast.Dict]]
+)
+
+
+def isnode(item: typing.Any) -> bool:
+    return isinstance(item, Node)
+
 
 @v_args(inline=True, meta=True)
 class SrpTransformer(Transformer):
-    TOKEN_TYPE: dict[str, str] = {}
+    def parse_container(self, meta: Meta, items: Tree) -> Containers:
+        if items.data == "dict":
+            keys: list[Node[typing.Any]] = []
+            values: list[Node[typing.Any]] = []
 
-    def parse_literals(self, meta: Meta, item: Token | Tree[Token]) -> Node[ast.Constant]:
+            for pair in typing.cast(list, items.children):
+                keys.append(pair[0])
+                values.append(pair[1])
+
+            return Literals.Dict(meta=meta, keys=keys, values=values)
+
+        types = {"list": Literals.List, "tuple": Literals.Tuple, "set": Literals.Set}
+
+        elements: list[Node] = []
+        for element in items.scan_values(isnode):
+            elements.append(element)
+
+        return types[items.data](meta=meta, elts=elements, ctx=ast.Load())
+
+    def key_value(self, _: Meta, key, value) -> tuple[Node[typing.Any], Node[typing.Any]]:
+        return key, value
+
+    def expression(self, _: Meta, item):
+        return item
+
+    list_literal = parse_container
+    tuple_literal = parse_container
+    set_literal = parse_container
+    dict_literal = parse_container
+
+    def parse_literals(
+        self, meta: Meta, item: Token | Tree[Token]
+    ) -> Node[type[ast.Constant]]:
         values: dict[str, bool | None] = {"true": True, "false": False}
 
         if isinstance(item, Token):
@@ -59,5 +98,8 @@ class SrpTransformer(Transformer):
     const_false = parse_literals
     const_true = parse_literals
 
-    def string(self, meta: Meta, token: Token) -> Node[ast.Constant]:
+    def const_string(self, _: Meta, string: Node[type[ast.Constant]]):
+        return string
+
+    def string(self, meta: Meta, token: Token) -> Node[type[ast.Constant]]:
         return Literals.Constant(value=token.value, meta=meta)
